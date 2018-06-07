19d93208166b09d5db88e05ffef5649e0c2ca959
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-07 18:05:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`; 
 }
 
-function holidayCountdown(days, x) {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`; 
 }
